9c01b2a549f78b06454b4c4c5548a2a13bcd3d62
Kyle Rummens <kyle@prodatakey.com>
2024-04-18 11:50:41 -0600

    Remove typo

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: 8 }}>
       <Link href="/journal-without-database">1. Journal without database</Link>
-      <Link href="/journal-with-database">2. eJournal with database</Link>
+      <Link href="/journal-with-database">2. Journal with database</Link>
     </div>
   )
 }
